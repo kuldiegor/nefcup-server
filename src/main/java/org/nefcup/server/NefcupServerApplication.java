ecd0775e7be50e85449a2c0ea073be5974c851cb
kuldiegor <support@kuldiegor.com>
2023-09-20 20:12:41 +0300

    init

---
+++ src/main/java/org/nefcup/server/NefcupServerApplication.java
@@ -0,0 +1,13 @@
+package org.nefcup.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NefcupServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NefcupServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/nefcup/server/NefcupServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nefcup.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NefcupServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
